7e5e039342af464bf62d61992d7cdc7114575f04
JeffreyHendricks <kiritoichigo1@gmail.com>
2018-01-20 23:10:25 -0500

    Lab 1 Final Commit

--- Lab1/script.js
+++ Lab1/script.js
@@ -1,4 +1,4 @@
 function sayHelloJS() {
-    document.getElementById("js").innerHTML = "Hello from JS";
-    document.getElementById("demo").innerHTML = "Hello From JS2";
+    document.getElementById("js").innerHTML = "Hello from JavaScript!";
+    document.getElementById("demo").innerHTML = "Hello From JavaScript too!";
 }
