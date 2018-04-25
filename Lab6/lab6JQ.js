ef0000a61337bd4911cf69da2254fde47a596eec
JeffreyHendricks <kiritoichigo1@gmail.com>
2018-04-24 22:47:14 -0400

    final commit

--- Lab6/lab6JQ.js
+++ Lab6/lab6JQ.js
@@ -1,22 +1,61 @@
 $(document).ready(function() {
 
 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function() {
-        if (xmlhttp.status == 200) {
-            responseObject = JSON.parse(xmlhttp.responseText);
+    var members = document.getElementById("members");
 
-            var newContent = "";
-            for (var i = 0; i < responseObject.members.length; i++) {
-                newContent += '<div class="members">';
-                newContent += '<p><b>' + responseObject.members[i].name + '""';
-                newContent += '</br>' + responseObject.members[i].address + '""';
-                newContent += '</br>' + responseObject.members[i].city + '""';
-                newContent += "</div>";
-            }
-            document.getElementById("members").innerHTML = newContent;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open('GET', 'http://mysafeinfo.com/api/data?list=englishmonarchs&format=json', true);
+
+    xmlhttp.onload = function() {
+
+        var responseObject = JSON.parse(xmlhttp.responseText);
+
+        if (xmlhttp.status == 200) {
+
+            myFunction(responseObject);
+
         }
+
+
     };
-    xmlhttp.open('GET', 'members.json', true);
-    xmlhttp.send(null);
+    xmlhttp.send();
+
+    function myFunction(data) {
+
+        var emptyString = "";
+
+        for (i = 0; i < data.length; i++) {
+
+            emptyString += "<p> The name of the person is " + "<b>" + data[i].nm + "</b>";
+            emptyString += "</br> and the city they are from is " + "<b>" + data[i].cty + "</b>";
+            emptyString += "</br> while the make of the house is " + "<b>" + data[i].hse + "</b>";
+            emptyString += "</br> and its years of age are from " + "<b>" + data[i].yrs + "</b>";
+            emptyString += "</p>";
+        }
+
+        members.innerHTML = emptyString;
+
+    }
+
+
+    $.ajax({
+        type: 'GET',
+        url: "levels.xml",
+        data: 'xml',
+        success: myFunction
+
+    });
+
+    function myFunction(xml) {
+
+        $(xml).find('level').each(function() {
+
+            var type = $(this).find("type").text();
+
+            $('members').html(type);
+
+        });
+    }
+
 });
